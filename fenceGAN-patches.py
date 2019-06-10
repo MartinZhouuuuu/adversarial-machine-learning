3c9a8e4b925aa0554f48cd8994ba1c5b91fbae30
zhoucy <zhoulele12@gmail.com>
2019-06-10 17:42:42 +0800

    added losses recording

--- fenceGAN-patches.py
+++ fenceGAN-patches.py
@@ -8,8 +8,10 @@ from keras.models import Sequential,Model
 from keras.optimizers import SGD, Adam, rmsprop
 from keras.layers.advanced_activations import LeakyReLU
 from keras.losses import binary_crossentropy
-import tensorflow as tf
 from keras.preprocessing.image import ImageDataGenerator
+import tensorflow as tf
+from keras import backend as K
+
 image_rows = 3
 image_columns = 3
 image_channels = 1
@@ -89,22 +91,30 @@ def train(GAN, G, D, epochs, v_freq=10):
 	discrimintator = build_discrimintator(image_shape)
 	generator = build_generator(latent_dim,image_shape)
 	GAN = fenceGAN()
-
 	noise = np.random.normal(0.5,0.5,(2000,100))
+	d_loss = np.empty((0,1))
+	g_loss = np.empty((0,1))
+
 	for epoch in range(epochs):
-		discrimintator.fit_generator(
+		history = discrimintator.fit_generator(
 		train_generator,
 		steps_per_epoch = 50,
 		epochs = 1
 		)
+		history_array = np.array([history.history['loss']])
+		d_loss = np.append(d_loss,history_array,axis=0)
+		
 		fake_label = np.zeros(2000)
 		fake_label[:] = 0.5
-		GAN.train_on_batch(noise,fake_label)
+		
+		generator_loss = GAN.train_on_batch(noise,fake_label)
+		print(generator_loss)
+		g_loss_array = np.array([[generator_loss]])
+		g_loss = np.append(g_loss,g_loss_array,axis = 0)
 
-	
+	print(d_loss)
+	print(g_loss)
 	fake_generated = generator.predict(noise)
-	validity = discrimintator.predict(fake_generated)
-	print(validity)
 	for x in range(fake_generated.shape[0]):
 		plt.imshow(fake_generated[x].reshape(3,3),cmap = 'gray')
 		#plt.imshow(fake_generated[0])
@@ -113,4 +123,6 @@ def train(GAN, G, D, epochs, v_freq=10):
 G = build_generator(latent_dim,image_shape)
 D = build_discrimintator(image_shape)
 GAN = fenceGAN()
-train(GAN, G, D, 1)
+train(GAN, G, D,10)
+K.clear_session()
+
