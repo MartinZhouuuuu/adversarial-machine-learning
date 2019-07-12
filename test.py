a0bb03d6af006fbb07ed6b652546e67bec328005
Zhou Chengyang <zhoulele12@gmail.com>
2019-07-12 16:20:19 +0800

    valid model for testing
    100%

--- test.py
+++ test.py
@@ -1,20 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from keras.preprocessing.image import img_to_array, array_to_img
+from keras.models import Sequential,Model,load_model
+from keras.preprocessing.image import img_to_array
 import tifffile
-import glob
-import cv2
+import os
+import random
 
-def get_dataset(path):
-	dataset = np.empty((0,28,28,1))
-	images = [tifffile.imread(file) for file in glob.glob(path)]
-	for img in images:
-		img_array = img_to_array(img)
-		dataset = np.append(dataset,[img_array],axis = 0)
-		
-	return dataset
-
-adversarial_dataset = get_dataset('full-fgsm/train/adversarial/*.tif')
-original_dataset = get_dataset('full-fgsm/train/original/*.tif')
-trainset = np.append(adversarial_dataset,original_dataset,axis = 0)
-print(trainset.shape)
+image = tifffile.imread(
+	'/Users/apple/Google Drive/adversarial-machine-learning/full-fgsm/test/adversarial/4884.tif'
+	)
+model = load_model('model-files/classifier.h5')
+prediction = model.predict(image.reshape(1,28,28,1))
+print(prediction)
